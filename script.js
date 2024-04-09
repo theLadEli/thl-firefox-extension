6f488687ba217f3f23196277be3d65e2f865907b
Eli <109231330+theLadEli@users.noreply.github.com>
2024-04-09 17:16:45 +0100

    Added notice for when the plugin is loaded

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
+// Add Plugin Loaded Notice
+
+var newDiv = document.createElement("div");
+newDiv.id = "plugin-loaded-notice";
+newDiv.innerText = "The CSS plugin has successfully loaded!";
+
+document.getElementById("login-container").appendChild(newDiv);
+
+// Apply Custom CSS
+
 var style = document.createElement('style');
 style.type = 'text/css';
 style.innerHTML = `
 
-    body {
-        background: red;
+    #plugin-loaded-notice {
+        margin-top: 25px;
+        padding: 15px 30px;
+        background-color: #c9fac8;
+        border-left: solid 3px #03a800;
+        font-size: 12px
     }
 
     @media only screen and (max-width: 600px) {
 
-        body {
-            background: green;
-        }
+        /* */
 
         .generic-component__content {
             flex-direction: column;
@@ -19,6 +31,10 @@ style.innerHTML = `
         .template-column {
             width: 100% !important;
             flex-direction: column-reverse;
+        }
+
+        #plugin-loaded-notice {
+            margin-bottom: 25px
         }
 
     }
